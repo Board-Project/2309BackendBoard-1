b8437813c2a5658f4674e3233cb7888bfe2173bf
eosl7808 <eosl7808@naver.com>
2023-12-03 02:29:32 +0900

    feature- User
    회원 로그인, 로그아웃, 회원 가입 인증 구현 v1 완료

---
+++ Board_Project/src/main/java/com/github/board/config/Security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.github.board.config.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain config(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .authorizeHttpRequests(
+                        authorize -> authorize
+                                .requestMatchers("/api/*").permitAll()
+                                .requestMatchers("/api/signup").permitAll()
+                                .requestMatchers("/api/login").permitAll()
+                                .requestMatchers("/api/logout").permitAll()
+
+                                .anyRequest().authenticated()
+                );
+
+
+
+        return http.build();
+    }
+}
