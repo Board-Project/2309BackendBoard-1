475153e0db8e038bf791b196012940391886a5dc
hyejinworld <117828146+hyejinworld@users.noreply.github.com>
2023-12-05 22:31:14 +0900

    feature-post-db삭제
    final test!

--- Board_Project/src/main/java/com/github/board/entity/Post.java
+++ Board_Project/src/main/java/com/github/board/entity/Post.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
-@Builder
+//@Builder
 @NoArgsConstructor
 @Table(name = "post")
 public class Post {
@@ -23,17 +23,11 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "user_id")
-    private Integer user_id;
-
     @Column(name = "title",length = 63, nullable = false)
     private String title;
 
     @Column(name = "content", length = 255, nullable = false)
     private String content;
-
-    @Column(name ="is_deleted")
-    private Integer is_deleted;
 
     @Column(name = "author", length = 32, nullable = false)
     private String author;
