55909bc0d0e077e96f2e83e2f61907d7fddfd155
eosl7808 <eosl7808@naver.com>
2023-12-07 19:40:12 +0900

    Swagger-import 및 config

--- Board_Project/src/main/java/com/github/board/web/controller/TestController.java
+++ Board_Project/src/main/java/com/github/board/web/controller/TestController.java
@@ -2,7 +2,7 @@ package com.github.board.web.controller;
 
 import com.github.board.service.TestService;
 import com.github.board.web.dto.articles.Article;
-import io.swagger.annotations.ApiOperation;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +20,7 @@ public class TestController {
     private final TestService testService;
 
 
-    @ApiOperation("모든 게시글 을 검색")
+
     @GetMapping("/articles")
     public List<Article> findAllArticles(){
         List<Article> articles=testService.findAllArticles();
