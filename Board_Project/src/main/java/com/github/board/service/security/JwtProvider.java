62b85ef3ada27e2e2530fcd63020324c6a345200
eosl7808 <eosl7808@naver.com>
2023-12-10 21:28:39 +0900

    feature- User 샘플 코드 삭제 및 주석 제거

--- Board_Project/src/main/java/com/github/board/service/security/JwtProvider.java
+++ Board_Project/src/main/java/com/github/board/service/security/JwtProvider.java
@@ -11,9 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
